17e936692bfe11d3512dde29eacee64c08c1bf66
Priya-nc <62901413+Priya-nc@users.noreply.github.com>
2021-10-10 13:48:55 +0530

    Add files via upload

--- IPL.java
+++ IPL.java
@@ -1,13 +1,43 @@
 import java.util.Scanner;
 class IPL{
+	static Scanner scan=new Scanner(System.in);
+	IPLTeams []ipl1=new IPLTeams[0];
 public static void main(String []arg){
-	Scanner scan=new Scanner(System.in);
-	System.out.println("Enter number of Teams ");
-	int n=scan.nextInt();
-	System.out.println("Enter Team details");
-	IPLTeams []ipl1=new IPLTeams[n];
-	for(int i=0;i<ipl1.length;i++){
+	
+	IPL ip=new IPL();
+	String more;
+	int n;
+	
+		
+		
+		
+		do{
+		System.out.println("To give team details type give, to display type display");
+		String inp=scan.nextLine();
+		if(inp.equals("give")){
+		    System.out.println("Enter Team details");
+			ip.give();}
+		else if (inp.equals("display")){
+			System.out.println("jhgf");
+			ip.display();}
+		else
+			System.out.println("enter valid input");
+			
+		
 		scan.nextLine();
+		System.out.println("you want to give more details type more");
+		more=scan.nextLine();
+		
+      }	while(more.equals("more"));
+}
+
+	 void give(){
+		System.out.println("Enter number of Teams ");
+		int n=scan.nextInt();
+		IPLTeams []ipl1=new IPLTeams[n];
+	for(int i=0;i<ipl1.length;i++){
+		if(i>0){
+		scan.nextLine();}
 		System.out.println("Enter Team Name ");
 		String teamName=scan.nextLine();
 		System.out.println("Enter number of Team members ");
@@ -17,6 +47,7 @@ public static void main(String []arg){
 		System.out.println("Enter the numeber of bollers in Team ");
 		byte numBollers=scan.nextByte();
 		
+		
 		IPLTeams ipl=new IPLTeams();
 		ipl.setTeamName(teamName);
 		ipl.setNumTeamMemeber(numTeamMemeber);
@@ -24,13 +55,19 @@ public static void main(String []arg){
 		ipl.setNumBollers(numBollers);
 		
 		ipl1[i]=ipl;
+		
+	  }
+	  
 	}
-	for(int i=0;i<ipl1.length;i++){
+	void display(){
+		for(int i=0;i<ipl1.length;i++){
 		IPLTeams ipl = ipl1[i];
 		System.out.println(ipl.getTeamName()+"\t"+ipl.getNumTeamMemeber()+"\t"+ipl.getnumBatsMan()+"\t"+ipl.getNumBollers());
+	  }
 	}
 	
-}
+
+
 }
 
 class IPLTeams{
